c539fac44726cf27881393460d873d142e11815d
piyushsanghi23 <piyushsanghi23@gmail.com>
2015-12-05 20:26:04 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -12,8 +12,49 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
+void update_d(int *Arr, int len, int c,int k)
+{
+	int i = 0;
+	for (i = c + 1; i < len; i++)
+	{
+		Arr[i] = Arr[k+1];
+		k++;
+	}
+}
+
 
 void * removeArrayDuplicates(int *Arr, int len)
 {
+	int c = 0, d = 0, swap = 0, n = len,k = 0,temp=0;
+	if (Arr && n > 0){
+		for (c = 0; c < (n - 1); c++)
+		{
+			for (d = 0; d < n - c - 1; d++)
+			{
+				if (Arr[d] > Arr[d + 1]) 
+				{
+					swap = Arr[d];
+					Arr[d] = Arr[d + 1];
+					Arr[d + 1] = swap;
+				}
+			}
+			
+		}
+		for (c = 0; c < (n - 1); c++)
+		{
+			temp = Arr[c];
+			k = c;
+			while (Arr[k + 1] == temp)
+			{
+				k++;
+			}
+			if (c != k)
+			{
+				update_d(Arr, len, c, k);
+			}
+		}
+		
+	}
+	else
 	return NULL;
 }
